f2d344221acade46837482133c0653d74993ee1d
Mukul Bhardwaj <bhardwaj.mukul2016@gmail.com>
2024-09-18 23:24:30 +0530

    mark task

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import todo_icon from '../assets/todo_icon.png'
 import Todoitems from './Todoitems'
 
@@ -31,6 +31,22 @@ const add=()=>{
         return prvTodos.filter((todo)=>todo.id !==id)
       })
     }
+
+    const toggle=(id)=>{
+      setTodoList((prevTodos)=>{
+        return prevTodos.map((todo)=>{
+          if(todo.id ===id){
+            return {...todo, isComplete: !todo.isComplete}
+          }
+          return todo;
+        })
+      })
+    }
+
+    useEffect(()=>{
+      console.log(todoList);
+    },[todoList])
+
   return (
     <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
 
@@ -54,7 +70,7 @@ const add=()=>{
 
        <div>
       {todoList.map((item,index)=>{
-        return <Todoitems key={index} text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo}/>
+        return <Todoitems key={index} text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo} toggle={toggle}/>
       })}
        </div>
 
--- src/components/Todoitems.jsx
+++ src/components/Todoitems.jsx
@@ -1,13 +1,13 @@
 import tick from '../assets/tick.png'
-// import not_tick from '../assets/not_tick.png'
+import not_tick from '../assets/not_tick.png'
 import delete_icon from '../assets/delete.png'
 const Todoitems = (prop) => {
   return (
     <div className='flex items-center my-3 gap-2'>
 
-      <div className='flex flex-1 items-center cursor-pointer'>
-        <img src={tick} alt="" className='w-7'/>
-        <p className='text-slate-700 ml-4 text-[17px]'>{prop.text}</p>
+      <div onClick ={()=>{prop.toggle(prop.id)}} className='flex flex-1 items-center cursor-pointer'>
+        <img src={prop.isComplete ? tick : not_tick} alt="" className='w-7'/>
+        <p className={`text-slate-700 ml-4 text-[17px] decoration-slate-500 ${prop.isComplete ? "line-through" : ""}`}>{prop.text}</p>
       </div>
 
       <img onClick={()=>{prop.deleteTodo(prop.id)}} src={delete_icon} alt="" className='w-3.5 cursor-pointer'/>
